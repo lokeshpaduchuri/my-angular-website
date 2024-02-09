d361cf2deee8110ed637885e4ffb110c43c497b5
lokesh <lokeshpaduchuri@gmail.com>
2024-02-09 16:24:11 -0500

    Updates to add header and needed chnages

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  pages = [
+    {
+      name: 'Photography',
+      url: '/photography'
+    },
+    {
+      name: 'Web Development',
+      url: '/developer'
+    },
+    {
+      name: 'About',
+      url: '/about-me'
+    },
+    {
+      name: 'Contact',
+      url: '/contact-me'
+    },
+]
 }
