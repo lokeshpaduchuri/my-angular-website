5b3b9833f36f54ae3253783f5397a074f5d17334
lokesh <lokeshpaduchuri@gmail.com>
2024-02-16 17:53:03 -0500

    Updating the color scheme of the site

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'; 
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
+  myForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if (this.myForm.valid) {
+      // Process the form data here (e.g., send it to a server)
+      console.log('Form submitted:', this.myForm.value);
+    } else {
+      // Handle form validation errors
+      console.error('Form is invalid. Please check the fields.');
+    }
+  }
 }
